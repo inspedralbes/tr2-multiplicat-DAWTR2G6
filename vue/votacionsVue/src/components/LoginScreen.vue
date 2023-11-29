dc7cd288d76b5c8b39c71bd1af38b0372696b22b
a22joslarfer <a22joslarfer@inspedralbes.cat>
2023-11-29 09:22:50 +0100

    vue in develop

---
+++ vue/votacionsVue/src/components/LoginScreen.vue
@@ -0,0 +1,61 @@
+<template>
+   
+<div class="surface-card p-4 shadow-2 border-round w-full lg:w-6">
+    <div class="text-center mb-5">
+        <div class="text-900 text-3xl font-medium mb-3">Welcome Back</div>
+        <span class="text-600 font-medium line-height-3">Don't have an account?</span>
+        <router-link to="/register" class="font-medium no-underline ml-2 text-blue-500 cursor-pointer">Create today!</router-link>
+    </div>
+
+    <div>
+        <label for="email1" class="block text-900 font-medium mb-2">Email</label>
+        <InputText id="email1" type="text" class="w-full mb-3" v-model="usuari"/>
+
+        <label for="password1" class="block text-900 font-medium mb-2">Password</label>
+        <InputText id="password1" type="password" class="w-full mb-3" v-model="password" />
+
+        <div class="flex align-items-center justify-content-between mb-6">
+            <div class="flex align-items-center">
+                <Checkbox id="rememberme1" :binary="true" v-model="checked" class="mr-2"></Checkbox>
+                <label for="rememberme1">Remember me</label>
+            </div>
+            <a class="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer">Forgot password?</a>
+        </div>
+ 
+
+
+            <Button label="Sign In" icon="pi pi-user" class="w-full" type="submit" v-on:click="login()">afs</Button>
+
+    </div>
+</div>
+</template>
+
+<script>
+    import {useAppStore} from '../stores/app'
+    export default {
+        data(){
+            return{
+                checked:false,
+                usuari:'',
+                password:''
+            } 
+        
+        },
+        methods:{
+            login() {
+        if (this.usuari === 'user' && this.password === '123') {
+            const appStore = useAppStore();
+            appStore.setLoginInfo(true, "Alvaro", "image.jpg");
+            console.log('Login successful:', appStore.getLoginInfo());
+            this.$router.push('/Juego');
+        } else {
+            console.error('Login failed');
+        }
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
