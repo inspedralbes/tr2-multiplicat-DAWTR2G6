159024076e79cc85d32aa7cc9908ac09e2e60145
a22joslarfer <a22joslarfer@inspedralbes.cat>
2023-12-15 13:01:08 +0100

    disseny_loginregister

--- vue/game/src/components/LoginScreen.vue
+++ vue/game/src/components/LoginScreen.vue
@@ -1,24 +1,20 @@
 <template>
   <div class="login">
-    
     <form @submit.prevent="login" class="form">
-      <h6 ><router-link to="/register">No tienes cuenta?</router-link></h6>
       <h2 class="title">Iniciar sesión</h2>
       <div class="group">
-        <label for="email">Email</label>
-        <input type="email" class="inputbox" id="email" v-model="email" required>
+        <input type="email" class="inputbox" placeholder="Email" v-model="email" required>
       </div>
       <div class="group">
-        <label for="password">Contraseña</label>
-        <input type="password" class="inputbox" id="password" v-model="password" required>
+        <input type="password" class="inputbox" placeholder="Contraseña" v-model="password" required>
       </div>
-      <button type="submit" class="btn btn-primary btn-login" :disabled="isLoading">
+      <button type="submit" class="btn btn-login" :disabled="isLoading">
         {{ isLoading ? 'Iniciando sesión...' : 'Iniciar sesión' }}
       </button>
+      <h6 class="register-link"><router-link to="/register">No tienes cuenta?</router-link></h6>
     </form>
   </div>
 </template>
-
 <script>
 export default {
   name: 'LoginScreen',
@@ -70,22 +66,15 @@ export default {
   },
 };
 </script>
-
-
-
-
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Anek+Bangla&display=swap');
-
 * {
   font-family: 'Anek Bangla', sans-serif;
-  border-radius: 15px;
   margin: 0;
 }
 
 body {
-  background-color: rgba(226, 222, 222, 0.815);
-  /*background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0)), url("/giphy.gif");*/
+  background-color: #0c0c28; /* Dark background color */
+  color: #f5f5f5; /* Light text color */
 }
 
 .login {
@@ -96,50 +85,63 @@ body {
 
 .title {
   font-size: 50px;
-
+  color: #ffffff;
 }
-
+a{
+  color: #dc9cec;
+}
 .form {
-  min-height: auto;
-  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.1);
-  border-radius: 3px;
-  text-align: center;
-  gap: 10px;
-  max-width: 400px;
-  background-color: #f5f5f5;
-  color: #1c1c1c;
-  padding: 80px;
-  width: 100%;
+  box-shadow: 0 0 10px rgba(255, 253, 253, 0.959);
+    border-radius: 10px;
+    text-align: center;
+    max-width: 400px;
+    background-color: #e5e5ec00;
+    padding: 20px;
+    width: 100%;
+    height: 600px;
 }
-.form h6{
-    text-decoration: underline;
-    position: relative;
-    left: -50%;
-    top: -44px;
-    font-size: 15px;
+.inputbox::placeholder {
+  color: #ffffffce;
 }
 .inputbox {
-  margin-bottom: 20px;
   outline: none;
-  width: 100%;
-  padding: 10px;
+    width: 73%;
+    padding: 10px;
+    background-color: #ebebf300;
+    border: none;
+    color: #ffffff;
+    margin-bottom: 10px;
+    border-bottom: 2px solid white;
+    position: relative;
+    font-size: 21px;
+    top: 39px;
+
+
 }
 
 .btn-login {
-  padding: 10px;
-  background-color: #1c1c1c;
-  color: #f5f5f5;
-  border: none;
-  cursor: pointer;
-  width: 106%;
-  margin-top: 20px;
-
-  transition: background-color 0.2s ease-out;
+  padding: 13px;
+    background-color: #f5f5f5;
+    color: #3b3a3a;
+    border: none;
+    cursor: pointer;
+    width: 80%;
+    transition: background-color 0.2s ease-out;
+    font-size: 21px;
+    top: 167px;
+    position: relative;
+    border-radius: 30px;
 }
 
 .btn-login:hover {
-  background-color: #000000;
+  background-color: #d095e7;
 }
 
-
+.register-link {
+  text-decoration: underline;
+    font-size: 18px;
+    margin-top: unset;
+    position: relative;
+    top: 179px;
+}
 </style>
