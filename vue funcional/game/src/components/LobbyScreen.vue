c0ac5560244905629a31a50a179ad4e3cc712d8b
Polete23 <a20poldonpoz@inspedralbes.cat>
2024-01-11 10:28:49 +0100

    fons

--- vue funcional/game/src/components/LobbyScreen.vue
+++ vue funcional/game/src/components/LobbyScreen.vue
@@ -1,17 +1,31 @@
 <template>
   <body>
+    <router-link to="/" custom v-slot="{ navigate }">
+      <button @click="navigate" role="link">HOME</button>
+    </router-link>
     <div class="lobby-container">
       <h2 class="lobby-title">Sala de Lobby</h2>
       <div class="lobby-options">
         <div class="lobby-game">
           <p>Selecciona el modo de juego:</p>
         </div>
-        <button @click="startSinglePlayerMode" class="btn" :disabled="multiPlayerModeStarted || mode === 'multiPlayer'">
-        
-          {{ multiPlayerModeStarted ? 'Deshabilitado hasta encontrar partida' : 'Vs Tu Mismo!' }}
+        <button
+          @click="startSinglePlayerMode"
+          class="btn"
+          :disabled="multiPlayerModeStarted || mode === 'multiPlayer'"
+        >
+          {{
+            multiPlayerModeStarted
+              ? "Deshabilitado hasta encontrar partida"
+              : "Vs Tu Mismo!"
+          }}
         </button>
-        <button @click="startMultiPlayerMode" class="btn" :disabled="multiPlayerModeStarted">
-          {{ multiPlayerModeStarted ? 'Buscando Partida...' : 'Multijugador' }}
+        <button
+          @click="startMultiPlayerMode"
+          class="btn"
+          :disabled="multiPlayerModeStarted"
+        >
+          {{ multiPlayerModeStarted ? "Buscando Partida..." : "Multijugador" }}
         </button>
       </div>
       <div v-if="mode === 'multiPlayer'" class="player-info">
@@ -23,15 +37,11 @@
         </div>
       </div>
     </div>
-
   </body>
 </template>
 
 <script>
-
-
 import { socket } from "../socket.js";
-
 
 export default {
   name: "LobbyScreen",
@@ -42,21 +52,17 @@ export default {
       players: [],
       catergoria: null,
       multiPlayerModeStarted: false,
-
     };
   },
   methods: {
     startSinglePlayerMode() {
-
-      if (this.mode === 'multiPlayer') {
-        socket.emit('salirdeMultijugador');
-
+      if (this.mode === "multiPlayer") {
+        socket.emit("salirdeMultijugador");
       }
       this.mode = "singlePlayer";
       if (this.mode === "singlePlayer") {
-        this.$router.push('/GameScreen');
+        this.$router.push("/GameScreen");
       }
-
     },
     startMultiPlayerMode() {
       this.multiPlayerModeStarted = true;
@@ -76,34 +82,30 @@ export default {
     socket.on("empezarJuego-mult", () => {
       console.log("Redireccionando a pantalla de juego");
       if (this.mode === "multiPlayer") {
-        this.$router.push('/GameScreenMult');
+        this.$router.push("/GameScreenMult");
       }
-
     });
   },
   beforeDestroy() {
     if (socket) {
       socket.disconnect();
     }
-
   },
 };
-
 </script>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Anek+Bangla&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Anek+Bangla&display=swap");
 
 body {
   margin-top: -48px;
-  font-family: 'Anek Bangla', sans-serif;
+  font-family: "Anek Bangla", sans-serif;
   height: 100vh;
   background-repeat: repeat;
   letter-spacing: 0.3rem;
   font-weight: lighter;
-  background: url("/public/f2.jpg");
+  background: url("/public/space_background.gif");
   background-repeat: no-repeat;
-  background-size:cover;
 }
 
 * {
@@ -115,14 +117,11 @@ body {
   align-items: center;
   justify-content: center;
   color: white;
-  
 }
 
 .lobby-game {
   text-shadow: 2px 2px 4px black;
 }
-
-
 
 .lobby-title {
   font-size: 100px;
@@ -134,7 +133,6 @@ body {
   display: grid;
   gap: 10px;
 }
-
 
 div p {
   margin-bottom: 10px;
@@ -153,8 +151,6 @@ div p {
   margin-top: 20px;
   transition: background-color 0.4s ease-out, font-size 0.2s ease-out;
   font-size: 25px;
-
-
 }
 
 .btn:hover {
@@ -168,11 +164,9 @@ div p {
 .player-info {
   margin-top: 20px;
   text-align: center;
-
 }
 
 @media (max-width: 1000px) {
-
   * {
     margin: 5%;
     justify-content: center;
@@ -181,7 +175,6 @@ div p {
   }
 
   .lobby-title {
-
     font-size: 60px;
   }
 
@@ -189,7 +182,6 @@ div p {
     display: flex;
     margin-left: auto;
     margin-right: auto;
-
   }
 }
 </style>
